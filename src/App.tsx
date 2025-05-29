4c2311f7248aaa50784e48090f693a6e88d2c007
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-29 18:19:42 -0300

    feat: adicionando novas props e tipagens ao componente CardProfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { CardProfile } from "./components/Card/CardProfile"
-
-
+import imagemPerfil from './assets/images/profile-img.jpeg';
 
 function App() {
 
   return (
     <div className="flex items-center justify-center h-screen bg-rose-200">
-      <CardProfile/>
+      <CardProfile nome="Sabrina" sobrenome="Souza" imagemPerfil={imagemPerfil} />
     </div>
   )
 }
--- src/components/Card/CardProfile.tsx
+++ src/components/Card/CardProfile.tsx
@@ -1,15 +1,19 @@
 import imageBg from '../../assets/images/img-bg.png';
-import imageProfile from '../../assets/images//profile-img.jpeg';
 import { SkillTag } from './SkillTag';
 
-export const CardProfile = () => {
+type CardProfileProps = {
+    nome: string;
+    sobrenome: string;
+    imagemPerfil?: string;
+}
+export const CardProfile = ({nome, sobrenome, imagemPerfil}:CardProfileProps) => {
   return (
     <section className="section-card">
       <div className="h-[147px] relative">
         <img src={imageBg} alt="" className="w-full h-full object-cover rounded-t-2xl" />
         <div className="absolute left-1/2 -translate-x-1/2 bottom-[-48px]">
           <img
-            src={imageProfile}
+            src={imagemPerfil}
             alt=""
             className="w-24 h-24 box-content object-cover rounded-full border-6 border-[#101828]"
           />
@@ -17,7 +21,7 @@ export const CardProfile = () => {
       </div>
       <div className="flex flex-custom text-center text-lg/tight">
         <div className="text-white/70 mt-16 flex flex-custom">
-          <h1 className="text-white font-bold text-2xl/tight">Sabrina Souza</h1>
+          <h1 className="text-white font-bold text-2xl/tight">{`${nome} ${sobrenome}`}</h1>
           <h2 className="font-bold">Desenvolvedora Front-End</h2>
           <p className="italic font-inter my-6 font-light">
             Cria Interfaces Bonitas, <br />Rápidas e Intuitivas.
