660ec6b71417c09004e36e75419d6d69e85cb5cf
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-29 13:39:03 -0300

    feat: adicionando imagem de fundo ao componente CardProfile

--- src/components/Card/CardProfile.tsx
+++ src/components/Card/CardProfile.tsx
@@ -1,8 +1,11 @@
+import imageBg from '../../assets/images/img-bg.png';
 
 export const CardProfile = () => {
   return (
     <section className="w-full sm:w-[368px] min-h-[427px] bg-amber-600 rounded-2xl">
-        CardProfile
+        <div className="">
+            <img src={imageBg} alt="" className='w-full h-[147px] object-cover' />
+        </div>
     </section>
   )
 }
