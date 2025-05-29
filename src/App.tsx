f25955a7929a70e408c0b99765f9760ac612ef97
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-29 13:32:29 -0300

    feat: Criando componente CardProfile e adicionando estilos inicias

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { CardProfile } from "./components/Card/CardProfile"
+
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <CardProfile/>
+    </div>
   )
 }
 
---
+++ src/components/Card/CardProfile.tsx
@@ -0,0 +1,8 @@
+
+export const CardProfile = () => {
+  return (
+    <section className="w-full sm:w-[368px] min-h-[427px] bg-amber-600 rounded-2xl">
+        CardProfile
+    </section>
+  )
+}
