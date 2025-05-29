6239ac4b1e482247f05c05d374a3b83be06bc051
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-29 14:31:13 -0300

    feat: Add infos pessoais

--- src/components/Card/CardProfile.tsx
+++ src/components/Card/CardProfile.tsx
@@ -7,7 +7,14 @@ export const CardProfile = () => {
         <div className="h-[147px] relative">
             <img src={imageBg} alt="" className="w-full h-full object-cover" />
             <div className="absolute left-1/2 -translate-x-1/2 bottom-[-48px]">
-                <img src={imageProfile} alt="" className='w-24 h-24 object-cover rounded-full' />
+                <img src={imageProfile} alt="" className='w-24 h-24 object-cover rounded-full border-6 border-[#101828]' />
+            </div>
+        </div>
+        <div className='flex justify-center items-center text-center text-lg'>
+            <div className='text-white/70 mt-16 flex flex-col justify-center items-center'>
+                <h1 className='text-white font-bold text-2xl'>Sabrina Souza</h1>
+                <h2 className='mb-6'>Desenvolvedora Front-End</h2>
+                <p className='w-5/6'>Cria interfaces bonitas, rápidas e intuitivas.</p>
             </div>
         </div>
     </section>
