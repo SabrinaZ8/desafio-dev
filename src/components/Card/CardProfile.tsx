573dfad3a72c300677141b6790d8aac13c3a0722
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-05-29 14:16:31 -0300

    feat: Adicionando imagem de perfil ao projeto

--- src/components/Card/CardProfile.tsx
+++ src/components/Card/CardProfile.tsx
@@ -1,10 +1,14 @@
 import imageBg from '../../assets/images/img-bg.png';
+import imageProfile from '../../assets/images//profile-img.jpeg';
 
 export const CardProfile = () => {
   return (
-    <section className="w-full sm:w-[368px] min-h-[427px] bg-amber-600 rounded-2xl">
-        <div className="">
-            <img src={imageBg} alt="" className='w-full h-[147px] object-cover' />
+    <section className="w-full sm:w-[368px] min-h-[427px] bg-[radial-gradient(at_right_bottom,_#80A9F9,_#101828_35%)] rounded-2xl">
+        <div className="h-[147px] relative">
+            <img src={imageBg} alt="" className="w-full h-full object-cover" />
+            <div className="absolute left-1/2 -translate-x-1/2 bottom-[-48px]">
+                <img src={imageProfile} alt="" className='w-24 h-24 object-cover rounded-full' />
+            </div>
         </div>
     </section>
   )
